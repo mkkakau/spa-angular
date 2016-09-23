45f0a186d299e1689ad65b9b1475e86bda591a86
Melanie A <mkkakau@gmail.com>
2016-09-22 18:07:58 -1000

    Changes to console log message

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -15,7 +15,7 @@
 			} else {
 				$scope.message = 'Enjoy!';
 			}
-			console.log($scope.items.length + ' item(s):');
+			console.log('Counted ' + $scope.items.length + ' item(s):');
 			console.log($scope.items);
 		};
 		function getItemsFromString(string) {
