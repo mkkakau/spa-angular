8b3471bed32d56bbc6d8037b3fe3fdb6c45c552b
Melanie A <mkkakau@gmail.com>
2016-09-22 18:01:29 -1000

    Implement functionality to LunchCheckController

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -4,7 +4,27 @@
 	.controller('LunchCheckController', LunchCheckController);
 
 	LunchCheckController.$inject = ['$scope'];
-	function LunchCheckController() {
-
+	function LunchCheckController($scope) {
+		$scope.items = [];
+		$scope.userInput = '';
+		$scope.message = '';
+		$scope.checkIfTooMuch = function () {
+			$scope.items = getItemsFromString($scope.userInput);
+			if($scope.items.length >= 3) {
+				$scope.message = 'Too much!';
+			} else {
+				$scope.message = 'Enjoy!';
+			}
+			console.log($scope.items.length + ' items:')
+			console.log($scope.items);
+		};
+		function getItemsFromString(string) {
+			var array = [];
+			array = string.split(',').filter(isValidItem);
+			return array;
+		}
+		function isValidItem(item) {
+			return item.trim() !== '';
+		}
 	}
 }());
